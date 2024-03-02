a9128e76d7d361e206d3c843b08363bfe6554248
Bohdan <bogmira777@gmail.com>
2024-03-02 21:21:55 +0100

    Style fixes and app's functionality enhancements

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 let cityForm;
 
-let temperatureContainer;
+let infoContainer;
+let temperatureLabel;
+let temperatureFeelsLikeLabel;
 let weatherDescriptionContainer;
 let locationContainer;
 let weatherIcon;
@@ -9,7 +11,11 @@ document.addEventListener("DOMContentLoaded", function () {
   cityForm = document.getElementById("cityForm");
   const cityInput = document.getElementById("city");
 
-  temperatureContainer = document.getElementById("temperature");
+  infoContainer = document.querySelector(".weather-card__content");
+  temperatureLabel = document.querySelector("#temperature span");
+  temperatureFeelsLikeLabel = document.querySelector(
+    "#temperatureFeelsLike span"
+  );
   weatherDescriptionContainer = document.getElementById("weatherDescription");
   locationContainer = document.getElementById("location");
   weatherIconContainer = document.getElementById("weatherIcon");
@@ -42,12 +48,18 @@ function fetchWeatherData(city) {
 }
 
 function displayWeatherData(data) {
+  infoContainer.classList.remove("hidden");
   const temperature = data.main.temp;
+  const temperatureFeelsLike = data.main["feels_like"];
+
+  console.log(data.main);
+
   const weatherDescription = data.weather[0].description;
   const weatherIcon = data.weather[0].icon;
   const location = data.name + ", " + data.sys.country;
 
-  temperatureContainer.textContent = temperature + "°C";
+  temperatureLabel.textContent = temperature + "°C";
+  temperatureFeelsLikeLabel.textContent = temperatureFeelsLike + "°C";
   weatherDescriptionContainer.textContent = weatherDescription;
   locationContainer.textContent = location;
   weatherIconContainer.innerHTML = `<img src="http://openweathermap.org/img/wn/${weatherIcon}.png" alt="Weather Icon" class="weather-icon">`;
